5f933ce3cd555696ef09569bf8674cdce57f3b0f
DinhThuan <dinhthuanvan@gmail.com>
2019-08-24 12:15:43 +0700

    update code

---
+++ reactjs-form/src/components/study-form/Form.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class Form extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <form>
+                    <div className="form-group mt-5">
+                        <label>Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <div className="form-group form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" >Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </React.Fragment>
+        );
+    }
+}
+
