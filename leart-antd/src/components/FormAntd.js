f31dbf2f9d853779ea8cb0aa09226ee33336c725
DinhThuan <dinhthuanvan@gmail.com>
2019-09-19 06:21:13 +0700

    antd

---
+++ leart-antd/src/components/FormAntd.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'antd';
+
+export default class FormAntd extends React.Component {
+    render() {
+        return (
+            <h1>Form component</h1>
+        );
+    }
+}
+
