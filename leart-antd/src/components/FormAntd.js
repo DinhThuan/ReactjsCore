87925dba331b3b9e7bda37f0218739bf0b7018f4
DinhThuan <dinhthuanvan@gmail.com>
2019-09-21 07:50:15 +0700

    update

--- leart-antd/src/components/FormAntd.js
+++ leart-antd/src/components/FormAntd.js
@@ -1,11 +1,57 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Form, Select, Input, Button } from 'antd';
+
+const { Option } = Select;
 
 export default class FormAntd extends React.Component {
-    render() {
-        return (
-            <h1>Form component</h1>
-        );
-    }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  handleSelectChange = value => {
+    console.log(value);
+    this.props.form.setFieldsValue({
+      note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form labelCol={{ span: 5 }} wrapperCol={{ span: 12 }} onSubmit={this.handleSubmit}>
+        <Form.Item label="Note">
+          {getFieldDecorator('note', {
+            rules: [{ required: true, message: 'Please input your note!' }],
+          })(<Input />)}
+        </Form.Item>
+        <Form.Item label="Gender">
+          {getFieldDecorator('gender', {
+            rules: [{ required: true, message: 'Please select your gender!' }],
+          })(
+            <Select
+              placeholder="Select a option and change input text above"
+              onChange={this.handleSelectChange}
+            >
+              <Option value="male">male</Option>
+              <Option value="female">female</Option>
+            </Select>,
+          )}
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 12, offset: 5 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
 }
 
+const WrappedApp = Form.create({ name: 'coordinated' })(FormAntd);
+
+// ReactDOM.render(<WrappedApp />, mountNode);
