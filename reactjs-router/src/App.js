79c5d1ff5ec19006129f37b7fd490be826d7245d
Đinh Văn Thuận - CMC Global DU1 <dvthuan@LDAP.CMCGLOBAL.COM.VN>
2019-11-04 15:02:33 +0700

    update code

--- reactjs-router/src/App.js
+++ reactjs-router/src/App.js
@@ -1,89 +1,50 @@
 import React from "react";
 import "./App.css";
-import {
-   BrowserRouter as Router,
-   Switch,
-   Route,
-   Link,
-   NavLink
-} from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Demo from "./components/Contact";
-import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Demo from "./components/Contact";
+// import NotFound from "./components/NotFound";
+import Menu from "./components/Menu";
+import routes from "./routes";
 
-const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
-   // console.log(activeOnlyWhenExact);
-   return (
-      <Route
-         path={to}
-         exact={activeOnlyWhenExact}
-         children={({ match }) => {
-            console.log(match);
-            let active = match ? "active" : "";
+class App extends React.Component {
+   showContentMenus = routes => {
+      let result = null;
+      if (routes.length > 0) {
+         result = routes.map((route, index) => {
+            console.log(route.main);
             return (
-               <li className={`nav-item ${active}`}>
-                  <Link className="nav-link" to={to}>
-                     {label}
-                  </Link>
-               </li>
+               <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+               />
             );
-         }}
-      />
-   );
-};
+         });
+      }
+      return result;
+   };
+   render() {
+      return (
+         <Router>
+            <div>
+               {/* menu */}
+               <Menu></Menu>
+               {/* content */}
 
-function App() {
-   return (
-      <Router>
-         <div>
-            {/* menu */}
-            <nav className="navbar navbar-expand-lg navbar-light bg-light  navbar-dark bg-dark">
-               <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav">
-                     {/* <li className="nav-item"> */}
-                     {/* <a className="nav-link" href="/">Home</a> */}
-                     {/* <Link className="nav-link" to="/">Home</Link> */}
-                     {/* <NavLink className="nav-link" to="/" activeClassName="active">Home</NavLink> */}
-                     {/* <NavLink className="nav-link" to="/" activeClassName="active" exact>Home</NavLink> */}
-                     <MenuLink label="Home" to="/" activeOnlyWhenExact={true} />
-                     {/* </li> */}
-                     {/* <li className="nav-item"> */}
-                     {/* <a className="nav-link" href="/about">About</a> */}
-                     {/* <Link  className="nav-link" to="/about">About</Link> */}
-                     {/* <NavLink className="nav-link" to="/about" activeClassName="active">About</NavLink> */}
-                     {/* <NavLink className="nav-link" to="/about" activeClassName="active">About</NavLink> */}
-                     <MenuLink
-                        label="About"
-                        to="/about"
-                        activeOnlyWhenExact={false}
-                     />
-                     {/* </li> */}
-                     {/* <li className="nav-item"> */}
-                     {/* <a className="nav-link" href="/contact">Contact</a> */}
-                     {/* <Link className="nav-link" to="/contact">Contact</Link> */}
-                     {/* <NavLink className="nav-link" to="/contact" activeClassName="active">Contact</NavLink> */}
-                     {/* <NavLink className="nav-link" to="/contact" activeClassName="active">Contact</NavLink> */}
-                     <MenuLink
-                        label="Contact"
-                        to="/contact"
-                        activeOnlyWhenExact={false}
-                     />
-                     {/* </li> */}
-                  </ul>
-               </div>
-            </nav>
-            {/* content */}
-
-            <Switch>
-               <Route path="/" exact component={Home}></Route>
-               <Route path="/about" exact component={About}></Route>
-               <Route path="/contact" component={Demo}></Route>
-               <Route path="*" exact={true} component={NotFound} />
-            </Switch>
-         </div>
-      </Router>
-   );
+               <Switch>
+                  {/* <Route path="/" exact component={Home}></Route>
+                  <Route path="/about" exact component={About}></Route>
+                  <Route path="/contact" component={Demo}></Route>
+                  <Route path="*" exact={true} component={NotFound} /> */}
+                  {this.showContentMenus(routes)}
+               </Switch>
+            </div>
+         </Router>
+      );
+   }
 }
 
 export default App;
--- reactjs-router/src/components/Menu.js
+++ reactjs-router/src/components/Menu.js
@@ -1,11 +1,93 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+
+const menus = [
+   {
+      name: "Home",
+      to: "/",
+      exact: true
+   },
+   {
+      name: "About",
+      to: "/about",
+      exact: false
+   },
+   {
+      name: "Contact",
+      to: "/contact",
+      exact: false
+   }
+];
+
+const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+   // console.log(activeOnlyWhenExact);
+   return (
+      <Route
+         path={to}
+         exact={activeOnlyWhenExact}
+         children={({ match }) => {
+            // console.log(match);
+            let active = match ? "active" : "";
+            return (
+               <li className={`nav-item ${active}`}>
+                  <Link className="nav-link" to={to}>
+                     {label}
+                  </Link>
+               </li>
+            );
+         }}
+      />
+   );
+};
 
 class Menu extends React.Component {
+   showMenu = menus => {
+      // console.log(menus);
+      let result = null;
+      if (menus.length > 0) {
+         result = menus.map((menu, index) => {
+            return (
+               <MenuLink
+                  key={index}
+                  label={menu.name}
+                  to={menu.to}
+                  activeOnlyWhenExact={menu.exact}
+               />
+            );
+         });
+      }
+      return result;
+   };
    render() {
       return (
-         <div>
-            <h4>Menu component</h4>
-         </div>
+         <nav className="navbar navbar-expand-lg navbar-light bg-light  navbar-dark bg-dark">
+            <div className="collapse navbar-collapse" id="navbarNav">
+               <ul className="navbar-nav">
+                  {/* <li className="nav-item"> */}
+                  {/* <a className="nav-link" href="/">Home</a> */}
+                  {/* <Link className="nav-link" to="/">Home</Link> */}
+                  {/* <NavLink className="nav-link" to="/" activeClassName="active">Home</NavLink> */}
+                  {/* <NavLink className="nav-link" to="/" activeClassName="active" exact>Home</NavLink> */}
+                  {/* <MenuLink label="Home" to="/" activeOnlyWhenExact={true} /> */}
+                  {/* </li> */}
+                  {/* <li className="nav-item"> */}
+                  {/* <a className="nav-link" href="/about">About</a> */}
+                  {/* <Link  className="nav-link" to="/about">About</Link> */}
+                  {/* <NavLink className="nav-link" to="/about" activeClassName="active">About</NavLink> */}
+                  {/* <NavLink className="nav-link" to="/about" activeClassName="active">About</NavLink> */}
+                  {/* <MenuLink label="About" to="/about" activeOnlyWhenExact={false}/> */}
+                  {/* </li> */}
+                  {/* <li className="nav-item"> */}
+                  {/* <a className="nav-link" href="/contact">Contact</a> */}
+                  {/* <Link className="nav-link" to="/contact">Contact</Link> */}
+                  {/* <NavLink className="nav-link" to="/contact" activeClassName="active">Contact</NavLink> */}
+                  {/* <NavLink className="nav-link" to="/contact" activeClassName="active">Contact</NavLink> */}
+                  {/* <MenuLink label="Contact" to="/contact" activeOnlyWhenExact={false} /> */}
+                  {/* </li> */}
+                  {this.showMenu(menus)}
+               </ul>
+            </div>
+         </nav>
       );
    }
 }
